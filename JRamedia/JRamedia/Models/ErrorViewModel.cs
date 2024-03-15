8eb9761e26366ed625c75af0f3f04aed12d5283f
JonathanRainee <jonatannnraineeee@gmail.com>
2024-03-15 17:04:41 +0700

    Init MVC
    Init MVC

---
+++ JRamedia/JRamedia/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JRamedia.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
