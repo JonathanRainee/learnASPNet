c3e1ffa67327693fd61210f41e2904e722e8de46
JonathanRainee <jonatannnraineeee@gmail.com>
2024-03-16 22:29:34 +0700

    create category
    create category

--- JRamedia/JRamedia/Controllers/CategoryController.cs
+++ JRamedia/JRamedia/Controllers/CategoryController.cs
@@ -17,11 +17,29 @@ namespace JRamedia.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
-        
+        //get
         public IActionResult Create()
         {
 
             return View();
+        }
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Name can't be the same with display order");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+                
+            }
+            return View(obj);
         }
 
 
