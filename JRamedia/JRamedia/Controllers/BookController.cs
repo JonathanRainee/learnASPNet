b1ee962d1a260cfc09f2d60f77bf4d872114135d
JonathanRainee <jonatannnraineeee@gmail.com>
2024-03-19 22:00:49 +0700

    add book controller
    add book controller

---
+++ JRamedia/JRamedia/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using JRamedia.Data;
+using JRamedia.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Permissions;
+
+namespace JRamedia.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        public BookController(ApplicationDBContext db)
+        {
+            _db = db;
+            
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Book> books = _db.Books;
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
