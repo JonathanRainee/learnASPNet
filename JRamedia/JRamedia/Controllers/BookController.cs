9cf9932a5831aef055675395c962e632e45d4757
JonathanRainee <jonatannnraineeee@gmail.com>
2024-03-21 20:53:07 +0700

    update book
    update book

--- JRamedia/JRamedia/Controllers/BookController.cs
+++ JRamedia/JRamedia/Controllers/BookController.cs
@@ -12,7 +12,7 @@ namespace JRamedia.Controllers
         public BookController(ApplicationDBContext db)
         {
             _db = db;
-            
+
         }
         public IActionResult Index()
         {
@@ -27,12 +27,12 @@ namespace JRamedia.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Book obj) { 
-            if(obj.Title.Length < 5)
+        public IActionResult Create(Book obj) {
+            if (obj.Title.Length < 5)
             {
                 ModelState.AddModelError("Title", "The title length can't be less than 5 characters");
             }
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 _db.Books.Add(obj);
                 _db.SaveChanges();
@@ -40,6 +40,36 @@ namespace JRamedia.Controllers
                 return RedirectToAction("Index");
             }
             return View(obj);
+        }
+
+        public IActionResult Update(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var BookFromDB = _db.Books.Find(id);
+            if(BookFromDB == null) { 
+                return NotFound();
+            }
+            return View(BookFromDB);
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Update(Book book) { 
+        
+            if (book.Title.Length < 5) {
+                ModelState.AddModelError("Title", "Title length can't be less than 5 characters");
+            }
+            if(ModelState.IsValid) { 
+                _db.Books.Update(book);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+
+            return View(book);
         }
     }
 }
