d72c92144e8161b98fd4ef6278b494f07473d902
JonathanRainee <jonatannnraineeee@gmail.com>
2024-04-17 00:21:26 +0700

    add book detail tp db
    add book detail tp db

---
+++ JRamedia/JRamedia/Migrations/20240416172046_AddBookDetailToDatabase.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JRamedia.Migrations
+{
+    public partial class AddBookDetailToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Langugae",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<float>(
+                name: "Length",
+                table: "Books",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Pages",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PublishDate",
+                table: "Books",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Publisher",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SerialNumber",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<float>(
+                name: "Weight",
+                table: "Books",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Width",
+                table: "Books",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Langugae",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "Length",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "Pages",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "PublishDate",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "Publisher",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "SerialNumber",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "Weight",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "Width",
+                table: "Books");
+        }
+    }
+}
--- JRamedia/JRamedia/Models/Books.cs
+++ JRamedia/JRamedia/Models/Books.cs
@@ -1,6 +1,7 @@
 ﻿using MessagePack;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
+using System.Security.Policy;
 
 namespace JRamedia.Models
 {
@@ -20,7 +21,22 @@ namespace JRamedia.Models
         [Required]
         public int Price { get; set; }
         public string? Image {  get; set; }
-
         public int BoughtTimes { get; set; }
+        [Required]
+        public int Pages { get; set; }
+        [Required]
+        public string Publisher { get; set; }
+        [Required]
+        public DateTime PublishDate { get; set; }
+        [Required]
+        public float Weight { get; set; }
+        [Required]
+        public string SerialNumber { get; set; }
+        [Required]
+        public float Width { get; set; }
+        [Required]
+        public float Length { get; set; }
+        [Required]
+        public string Langugae { get; set; }
     }
 }
